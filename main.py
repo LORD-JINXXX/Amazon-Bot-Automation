f966dcf923242862dbec35804c146f2b1b9b3982
Mustafa Mohammed Sajid <113443942+LORD-JINXXX@users.noreply.github.com>
2023-04-20 01:55:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from Bot.bot import Bot
+
+with Bot() as bot:
+    bot.lan_Amazon()
+    bot.maxwin()
+    bot.menu_btn()
+    bot.sign_btn()
+    bot.username()
+    bot.password()
+    bot.search()
+    bot.review(4)
+    bot.price_set(500, 1000)
+    bot.select_item(3)
+    bot.switch()
+    bot.add_to_cart()
